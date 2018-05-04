be20804bd035fdfd6959f932dfe7612576274140
Pavel Solikov <psolikov15@gmail.com>
2018-05-04 06:57:10 +0300

    Deleted tests with empty folder

--- FTP/src/test/java/ru/spbau/solikov/ftp/test/FTPTest.java
+++ FTP/src/test/java/ru/spbau/solikov/ftp/test/FTPTest.java
@@ -51,18 +51,13 @@ public class FTPTest {
     }
 
     @Test
-    public void testListOnEmptyDirectory() throws IOException {
-        assertEquals(client.list("src/test/resources/EmptyDir").size(), 0);
-    }
-
-    @Test
     public void testGetOnNonExistentFile() throws IOException {
         assertEquals(client.list("src/test/resources/dqofpko").size(), 0);
     }
 
     @Test
     public void testGetOnDirectory() throws IOException {
-        assertEquals(client.list("src/test/resources/EmptyDir").size(), 0);
+        assertEquals(client.list("src/test/resources/DirWith4Files").size(), 0);
     }
 
     @Test
