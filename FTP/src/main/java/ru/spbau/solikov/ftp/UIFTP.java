903a0dfafdbb875b2a6b3d5551b0fd818c79c347
Pavel Solikov <psolikov15@gmail.com>
2018-06-21 11:15:21 +0300

    Fixed remarks

--- FTP/src/main/java/ru/spbau/solikov/ftp/UIFTP.java
+++ FTP/src/main/java/ru/spbau/solikov/ftp/UIFTP.java
@@ -1,25 +1,24 @@
 package ru.spbau.solikov.ftp;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * A user interface of frp-client that allows to traverse and download files from directories.
- * To go to the parent folder use "/.." button. When you want to download some file from current folder, hit
- * "download" button and after that select a file or hit "download" button again to continue browsing.
+ * To go to the parent folder use "Up Folder" button. When you want to download some file from current folder, hit
+ * "Download/Browse" button and after that select a file or hit "Download/Browse" button again to continue browsing.
  */
 public class UIFTP extends Application {
 
@@ -27,109 +26,121 @@ public class UIFTP extends Application {
     private Path current;
     private BorderPane root;
     private Button download;
-    private VBox pathsBox = new VBox();
-    private Map<Button, FTPFile> map = new HashMap<>();
+    private TableView<FTPFile> table = new TableView<>();
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        List<String> parameters = getParameters().getRaw();
-        client = new FTPClient(Integer.parseInt(parameters.get(0)), parameters.get(1));
+    public void start(Stage primaryStage) {
+        TextField address = new TextField();
+        address.setPromptText("Address:");
+        TextField port = new TextField();
+        port.setPromptText("Port:");
+        Button submit = new Button("Submit");
+        submit.setOnAction(e -> {
+            try {
+                client = new FTPClient(Integer.parseInt(address.getText()),
+                        port.getText());
+                setUpApp(primaryStage);
+            } catch (NumberFormatException n) {
+                showAlert("Bad format. Try again.");
+            } catch (IOException ex) {
+                showAlert("Can't connect to the server. Try again.");
+            }
+        });
+        address.setMaxSize(200, 10);
+        port.setMaxSize(200, 10);
+        VBox vBox = new VBox(submit, address, port);
+        vBox.setAlignment(Pos.CENTER);
+        Scene connecting = new Scene(vBox, 500, 500);
         primaryStage.setTitle("FTP Client");
-        pathsBox.setSpacing(5);
-        root = new BorderPane();
-        download = new Button("Download");
-        download.setAlignment(Pos.CENTER);
-        download.setOnAction(event -> setButtonsDownload(getButtons()));
-        root.setTop(download);
-        BorderPane.setAlignment(download, Pos.CENTER);
-        Scene scene = new Scene(root, 500, 500);
-        current = Paths.get(System.getProperty("user.dir"));
-        setScene(current);
-        primaryStage.setScene(scene);
+        primaryStage.setScene(connecting);
         primaryStage.show();
     }
 
-    private void setButtonsBrowse(Collection<Button> buttons) {
-        download.setStyle("");
-        download.setOnAction(event -> setButtonsDownload(getButtons()));
-        for (Button button : buttons) {
-            if (map.get(button).isDirectory()) {
-                button.setStyle("-fx-text-fill: #0064cc;");
-                button.setOnAction(event -> {
-                    reBuild(map.get(button).getName());
-                });
-                continue;
-            }
-            button.setStyle("");
-            button.setOnAction(event -> {
-            });
-        }
-    }
-
-    private void setButtonsDownload(Collection<Button> buttons) {
-        download.setStyle("-fx-background-color: rgba(43,46,53,0.19)");
-        download.setOnAction(event -> setButtonsBrowse(getButtons()));
-
-        for (Button button : buttons) {
-            if (map.get(button).isDirectory()) {
-                button.setOnAction(event -> {
-                });
-                button.setStyle("-fx-background-color: rgba(43,46,53,0.19)");
-                continue;
-            }
-            button.setOnAction(event -> {
-                try {
-                    client.get(current + "/" + map.get(button).getName());
-                } catch (IOException e) {
-                    showAlert("Sorry, can't download the file : " + e.getMessage());
-                }
-            });
-        }
-    }
-
-    private void setScene(Path path) {
-        List<Button> collect = new ArrayList<>();
-        Button upFolder = new Button("/..");
-        upFolder.setOnAction(event -> {
+    private void setUpApp(Stage primaryStage) {
+        primaryStage.setTitle("FTP Client");
+        table.setEditable(true);
+        table.setPrefSize(500, 500);
+        TableColumn<FTPFile, String> files = new TableColumn<>("Files");
+        TableColumn<FTPFile, String> isDir = new TableColumn<>("Is directory");
+        files.setCellValueFactory(new PropertyValueFactory<>("name"));
+        files.setPrefWidth(250);
+        isDir.setCellValueFactory(new PropertyValueFactory<>("dir"));
+        isDir.setPrefWidth(250);
+        table.getColumns().add(files);
+        table.getColumns().add(isDir);
+        download = new Button("Download");
+        Button up = new Button("Up Folder");
+        download.setAlignment(Pos.CENTER);
+        up.setAlignment(Pos.CENTER);
+        download.setOnAction(event -> setDownload());
+        up.setOnAction(event -> {
             stepBack();
             reBuild("");
         });
-        collect.add(upFolder);
+        HBox hBox = new HBox(download, up);
+        hBox.setAlignment(Pos.CENTER);
+        BorderPane.setAlignment(download, Pos.CENTER);
+        root = new BorderPane();
+        root.setTop(hBox);
+        current = Paths.get(System.getProperty("user.dir"));
+        setScene(current);
+        setBrowse();
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
+    }
+
+    private void setDownload() {
+        download.setStyle("-fx-background-color: rgba(43,46,53,0.19)");
+        download.setOnAction(event -> setBrowse());
+        download.setText("Browse");
+        table.setOnMouseClicked(e -> {
+            FTPFile ftp = table.getSelectionModel().getSelectedItem();
+            if (ftp == null) return;
+            if (!ftp.getDir()) {
+                try {
+                    client.get(current + "/" + ftp.getName());
+                    showAlert("Downloaded!");
+                } catch (IOException ex) {
+                    showAlert("Sorry, can't download the file : " + ex.getMessage());
+                }
+            }
+        });
+    }
+
+    private void setBrowse() {
+        download.setText("Download");
+        download.setStyle("");
+        download.setOnAction(event -> setDownload());
+        table.setOnMouseClicked(e -> {
+            FTPFile ftp = table.getSelectionModel().getSelectedItem();
+            if (ftp == null) return;
+            if (ftp.getDir()) {
+                reBuild(ftp.getName());
+            }
+        });
+    }
+
+    private void setScene(Path path) {
         try {
-            collect.addAll(client.list(path.toString()).stream().map(f ->
-            {
-                Button b = new Button(f.getName());
-                if (f.isDirectory()) b.setStyle("-fx-text-fill: #0064cc;");
-                map.put(b, f);
-                return b;
-            })
-                    .collect(Collectors.toList()));
+            table.setItems(FXCollections.observableArrayList(client.list(path.toString())));
         } catch (IOException e) {
-            showAlert("Sorry, can't get a list of files : " + e.getMessage());
+            showAlert("Can't get list of files");
+            e.printStackTrace();
         }
-        pathsBox.getChildren().addAll(collect);
-        setButtonsBrowse(getButtons());
-        ScrollPane scrollPane = new ScrollPane(pathsBox);
-        scrollPane.setPrefSize(500, 500);
-        root.setCenter(scrollPane);
+        root.setCenter(table);
     }
 
     private void stepBack() {
         if (current.getParent() != null) current = current.getParent();
     }
 
-    private Collection<Button> getButtons() {
-        return map.keySet();
-    }
-
     private void reBuild(String name) {
-        pathsBox.getChildren().clear();
-        map.clear();
         current = Paths.get(current + "/" + name);
         setScene(current);
     }
 
-    private void showAlert(String message){
+    private void showAlert(String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setContentText(message);
         alert.showAndWait();
